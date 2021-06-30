d96a07170654b1bfaf7d7249ca433cce0d470603
alyAds <aliarmandaulay62@gmail.com>
2021-06-30 23:01:33 +0700

    bulan sudah sejajar

--- javascript/season.js
+++ javascript/season.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
 	let windowHeight = $(window).height();
 	let satuanDeg = windowHeight / 360;
+	let countSeason = 1;
+	let moon = $("#moon");
 	let seasons = ["night", "noon", "noonRain", "nightRain"];
 
 	for (var i = 0; i < seasons.length; i++) {
-		$('<div id="trs-'+i+'" data-trs="'+seasons[i]+'" style="height: '+windowHeight+'px"></div>').insertBefore('#sec')
+		$('<div id="'+seasons[i]+'" style="height: '+windowHeight+'px"></div>').insertBefore('#sec')
 	}
 
+	$("#stars").clone().appendTo('#nightRain');
+
 	$(window).scroll(function(event) {
-		// let stars = ;
-		// let mountains_behind_malam = document.getElementById('mountains_behind_malam');
-		// let mountains_front_malam = document.getElementById('mountains_front_malam');
 		let valScrlTop = $(this).scrollTop();
-		satuanDeg = satuanDeg + ((windowHeight - valScrlTop) / 360);
+		$("#efg").text(valScrlTop)
+
+		$("#abc").text((valScrlTop/windowHeight))
+		
+		if (valScrlTop <= $("#night").offset().top || valScrlTop >= $("#nightRain").offset().top) {
+			moon.css('transform', 'rotate(-'+(valScrlTop / (satuanDeg*3.25)) * satuanDeg+'deg) translate(0px, -100px)');
+
+			countSeason = (countSeason > 0) ? countSeason-- : 0;
+		} else {
+			countSeason++;
+		}
 
 		$('#stars').css('bottom', '-'+valScrlTop * 0.25 + 'px');
-		// mountains_behind_malam.style.top = valScrlTop * 0.5 + 'px';
-		// mountains_front_malam.style.top = valScrlTop * 0 + 'px';
-		$('#moon').css('transform', 'rotate(-'+satuanDeg+'deg)');
 		$('#text').css('margin-right', valScrlTop * 4 + 'px');
 		$('#text').css('margin-top', valScrlTop * 1.5 + 'px');
 		$('#btn').css('margin-top', valScrlTop * 1.5 + 'px');
