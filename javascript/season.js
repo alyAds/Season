7756f7ca411d9d7a55a59190d75e2c2af7e7feab
alyAds <aliarmandaulay62@gmail.com>
2021-07-01 17:51:09 +0700

    semoga kali ini posisi bulannya sejajar selalu T.T

--- javascript/season.js
+++ javascript/season.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
-	let windowHeight = $(window).height();
-	let satuanDeg = windowHeight / 360;
-	let countSeason = 1;
+	let windowHeight = $(window).outerHeight();
+	let satuanDeg = windowHeight / 185;
+	let roundSatuanDeg = Math.floor(satuanDeg);
+	let decimalsSatuanDeg = satuanDeg - roundSatuanDeg;
 	let moon = $("#moon");
 	let seasons = ["night", "noon", "noonRain", "nightRain"];
+	
+	satuanDeg = roundSatuanDeg - decimalsSatuanDeg;
+
+	// const round = (number, decimalPlaces) => {
+	// 	const factorOfTen = Math.pow(10, decimalPlaces)
+	// 	return Math.round(number * factorOfTen) / factorOfTen
+	// }
 
 	for (var i = 0; i < seasons.length; i++) {
-		$('<div id="'+seasons[i]+'" style="height: '+windowHeight+'px"></div>').insertBefore('#sec')
+		$('#'+seasons[i]).height(windowHeight);
+
 	}
 
 	$("#stars").clone().appendTo('#nightRain');
 
 	$(window).scroll(function(event) {
 		let valScrlTop = $(this).scrollTop();
-		$("#efg").text(valScrlTop)
+		const nightYaxis = document.getElementById("night").getBoundingClientRect().top;
+		let a = (($("body").height() / $(this).outerHeight()) * satuanDeg) - satuanDeg;
 
-		$("#abc").text((valScrlTop/windowHeight))
-		
+		$("#hij").text(nightYaxis)
+		$("#efg").text(satuanDeg)
+
+		$("#abc").text(a)
+
 		if (valScrlTop <= $("#night").offset().top || valScrlTop >= $("#nightRain").offset().top) {
-			moon.css('transform', 'rotate(-'+(valScrlTop / (satuanDeg*3.25)) * satuanDeg+'deg) translate(0px, -100px)');
-
-			countSeason = (countSeason > 0) ? countSeason-- : 0;
-		} else {
-			countSeason++;
+			moon.css('transform', 'rotate(-'+(valScrlTop / a)  * satuanDeg+'deg) translate(0px, -100px)');
+			// moon.css('transform', 'rotate(-'+(valScrlTop / (satuanDeg*($(document).height() / windowHeight))) * satuanDeg+'deg) translate(0px, -100px)');
 		}
 
 		$('#stars').css('bottom', '-'+valScrlTop * 0.25 + 'px');
