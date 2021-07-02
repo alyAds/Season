66aff96d0c26fe904e137c55dbf73a4a974725e6
alyAds <aliarmandaulay62@gmail.com>
2021-07-02 17:53:12 +0700

    mountain and sun are finish before noon rain

--- javascript/season.js
+++ javascript/season.js
@@ -1,43 +1,84 @@
 $(document).ready(function() {
-	let windowHeight = $(window).outerHeight();
-	let satuanDeg = windowHeight / 185;
-	let opacityNight = 1;
-	let moon = $("#moon");
-	let seasons = ["night", "noon", "noonRain", "nightRain"];
-	
-	// satuanDeg = roundSatuanDeg - decimalsSatuanDeg;
+	const moon = $("#moon");
+	const sun = $("#sun");
+	const stars = $("#stars");
+	const mountainsMalam = $(".mountains_malam");
+	const mountainsSiang = $(".mountains_siang");
+	const seasons = ["night", "noon", "noonRain", "nightRain"];
 
-	// const round = (number, decimalPlaces) => {
-	// 	const factorOfTen = Math.pow(10, decimalPlaces)
-	// 	return Math.round(number * factorOfTen) / factorOfTen
-	// }
+	let windowHeight = $(window).outerHeight();
+	let valScrlTop = $(window).scrollTop();
 
 	for (var i = 0; i < seasons.length; i++) {
 		$('#'+seasons[i]).height(windowHeight);
 	}
 
-	$("#stars").clone().appendTo('#nightRain');
+	const night = document.getElementById('night');
+	const noon = document.getElementById('noon');
+	const noonOffsetTop = $('#noon').offset().top;
+	const noonRain = document.getElementById('noonRain');
+
+	let noonAxisY = seasonAxisY(noon);
+
+	stars.clone().appendTo('#nightRain');
+
+	seasonNight($(window), night, moon, stars, mountainsMalam)
+	noonSunPosition(valScrlTop, noonAxisY, windowHeight, sun)
+
+	$(window).resize(function(event) {
+		seasonNight($(this), night, moon, stars, mountainsMalam)
+		noonSunPosition(valScrlTop, noonAxisY, windowHeight, sun)
+	})
 
 	$(window).scroll(function(event) {
-		let valScrlTop = $(this).scrollTop();
-		const nightYaxis = document.getElementById("night").getBoundingClientRect().top;
-		// let a = (($("body").height() / $(this).outerHeight()) * 2 ) ;
-
-		opacityNight = (((nightYaxis/2) / windowHeight) * 100) / 100;
+		windowHeight = $(window).outerHeight(); 
+		valScrlTop = $(this).scrollTop();
+		noonAxisY = seasonAxisY(noon);
+		let noonRainAxisY = seasonAxisY(noonRain);
 		
-		// $("#abc").text(valScrlTop)
-		$("#hij").text(opacityNight)
-		$("#efg").text(nightYaxis)
+		// $("#abc").text(windowHeight*(22/100))
+		// $("#hij").text((((6/100) * noonOffsetTop) + windowHeight))
+		$("#efg").text(valScrlTop)
 
+		seasonNight($(this), night, moon, stars, mountainsMalam)
 
+		if (valScrlTop > (noonAxisY - (windowHeight*(60/100))) && valScrlTop < ((noonAxisY + (windowHeight*(110/100))) - (windowHeight*(1/400)))) {
+			let opacityNight = opacitySeason(noonAxisY, windowHeight) - 0.3;
 
-		if (nightYaxis > (windowHeight*(22/100))) {
-			moon.css('bottom', (nightYaxis - (windowHeight*(30/100)))+ 'px');
+			sun.css('top', (noonAxisY - (windowHeight*(60/100)))+ 'px');
+
+			mountainsMalam.css('opacity', opacityNight);
 		}
-
-		$('#stars').css('bottom', '-'+valScrlTop * 0.25 + 'px');
-		$('#text').css('margin-right', valScrlTop * 4 + 'px');
-		$('#text').css('margin-top', valScrlTop * 1.5 + 'px');
-		$('#btn').css('margin-top', valScrlTop * 1.5 + 'px');
 	});
 });
+
+function seasonNight(WINDOW, night, moon, stars, mountainsMalam) {
+	let windowHeight = $(WINDOW).outerHeight(); 
+	let valScrlTop = $(WINDOW).scrollTop();
+	let nightAxisY = seasonAxisY(night);
+	let opacityNight = opacitySeason(nightAxisY, windowHeight);
+
+	if (nightAxisY > (windowHeight*(22/100))) {
+		moon.css('top', (nightAxisY - (windowHeight*(60/100)))+ 'px');
+	}
+
+	stars.css({
+		opacity: opacityNight,
+		bottom: '-'+valScrlTop * 0.25 + 'px'
+	});
+	
+}
+
+function noonSunPosition(valScrlTop, noonAxisY, windowHeight, sun) {
+	if (valScrlTop < noonAxisY) {
+		sun.css('top', ((noonAxisY/2) - (windowHeight*(30/100)))+ 'px');
+	}
+}
+
+function opacitySeason(seasonAxisY, windowHeight) {
+	return ((seasonAxisY / windowHeight) * 100) / 100;
+}
+
+function seasonAxisY(season) {
+	return season.getBoundingClientRect().top;
+}
